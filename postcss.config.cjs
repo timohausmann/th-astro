a5990d1eb5ed0ab6ad6b418fe11fd340d84b9af7
Timo Hausmann <hello.timohausmann@gmail.com>
2025-05-29 16:51:38 +0200

    custom-media

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,17 @@
-module.exports = {
-    plugins: [require("postcss-pxtorem")],
+/** @type {import('postcss-load-config').Config} */
+const config = {
+    plugins: [
+        require("@csstools/postcss-global-data")({
+            files: ["./src/css/breakpoints.css"],
+        }),
+        require("postcss-preset-env")({
+            features: {
+                "custom-media-queries": true,
+            },
+        }),
+        require("postcss-pxtorem"),
+        require("autoprefixer"),
+    ],
 };
+
+module.exports = config;
